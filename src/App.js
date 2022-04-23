2d1721c25ae5757cc59a3da8ed120da7c48a8c36
DavidXie10 <davidxieli@gmail.com>
2022-04-22 21:24:39 -0600

    Removed unnecesary data

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ export default function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    Tarea #1
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                Learn React
-                </a>
                 <h1 className="text-3xl font-bold underline">
                     David Xie Li (B88682)
                 </h1>
